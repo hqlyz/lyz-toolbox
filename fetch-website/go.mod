f9af022f4b8add66455b59fc61d478c70ee70dad
lyz <lyz@gmail.com>
2021-03-16 17:18:04 +0800

    update

--- fetch-website/go.mod
+++ fetch-website/go.mod
@@ -1,3 +1,5 @@
 module fetch-website
 
 go 1.15
+
+require github.com/PuerkitoBio/goquery v1.6.1 // indirect
--- fetch-website/main.go
+++ fetch-website/main.go
@@ -1,19 +1,59 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"net/url"
+	"os"
+	"path"
+	"strings"
 	"time"
+
+	"github.com/PuerkitoBio/goquery"
 )
 
+var (
+	urlProtocol = "http://"
+)
+
+type staticFile struct {
+	fileURL  string
+	filePath string
+}
 
 func main() {
+	if len(os.Args) < 2 {
+		fmt.Printf("Usage: fetch-website some-url")
+		return
+	}
+	webURL := os.Args[1]
+	if strings.Contains(webURL, "https") {
+		urlProtocol = "https://"
+	}
+	u, err := url.Parse(webURL)
+	// 确保传入的URL是合法的URL
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// 新建文件夹存储所有文件
+	rootDir := u.Host
+	if !fileExists(rootDir) {
+		err = os.Mkdir(rootDir, 0666)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
 	log.Println("Start fetching website...")
-	cli := &http.Client {
+	cli := &http.Client{
 		Timeout: time.Second * 5,
 	}
-	req, err := http.NewRequest("GET", "http://wendu.cn", nil)
+	// construct a http request
+	req, err := http.NewRequest("GET", webURL, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -23,15 +63,75 @@ func main() {
 	}
 	defer resp.Body.Close()
 
-	buf, err := ioutil.ReadAll(resp.Body)
+	if resp.StatusCode != 200 {
+		log.Fatalf("Fetch website failed with status: %s", resp.Status)
+	}
+
+	var buf = bufio.NewReader(resp.Body)
+
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	err = ioutil.WriteFile("test.html", buf, 0666)
+	staticFiles := []staticFile{}
+
+	doc.Find("link, script").Each(func(i int, s *goquery.Selection) {
+		href, exist := s.Attr("href")
+		if exist {
+			fileURL := getStaticFilePath(urlProtocol, u, href)
+			if fileURL != "" {
+				staticFiles = append(staticFiles, staticFile{fileURL: fileURL, filePath: href})
+			}
+		} else {
+			src, exist := s.Attr("src")
+			if exist {
+				fileURL := getStaticFilePath(urlProtocol, u, src)
+				if fileURL != "" {
+					staticFiles = append(staticFiles, staticFile{fileURL: fileURL, filePath: src})
+				}
+			}
+		}
+	})
+
+	log.Println("Need to be downloaded...")
+	for _, s := range staticFiles {
+		log.Println(s.fileURL, s.filePath)
+	}
+
+	buf.Reset(resp.Body)
+	contentBytes, err := ioutil.ReadAll(buf)
+	log.Println("content length: ", len(contentBytes))
 	if err != nil {
 		log.Fatal(err)
 	}
+	err = ioutil.WriteFile(path.Join(rootDir, "index.html"), contentBytes, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
 
-	log.Println("Done")
+func getStaticFilePath(protocol string, u *url.URL, src string) string {
+	if len(src) == 0 {
+		return ""
+	}
+	if src[0] == '/' {
+		return path.Join(protocol, u.Host, src)
+	}
+	return path.Join(protocol, u.Host, u.Path, src)
+}
+
+func downloadStaticFile(fileURL string, filePath string) {
+
+}
+
+func fileExists(path string) bool {
+	_, err := os.Stat(path)
+	if err != nil {
+		if os.IsExist(err) {
+			return true
+		}
+		return false
+	}
+	return true
 }
